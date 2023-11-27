b2518bfbee58a63b28100dfd2e7c3787a0883088
mahmoud <mahmoud.shabban469@gmail.com>
2023-11-27 19:38:33 +0200

    python

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[str.index('ob'):str.index(' l')])
+print(str[str.index('ob'):str.index(' l')], end = ' ')
+print(str[str.index('with'):str.index('with') + 4], str[:7], sep = ' ')
