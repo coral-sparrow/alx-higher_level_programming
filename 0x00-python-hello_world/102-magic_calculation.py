d0be7131811b926b41711ff13f381f91f6d9648a
mahmoud <mahmoud.shabban469@gmail.com>
2023-11-27 20:21:05 +0200

    python

---
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+	result = a ** b  # Equivalent to BINARY_POWER in bytecode
+    	result += 98     # Equivalent to BINARY_ADD in bytecode
+    	return result
