69f37433ccca430290eab10ea279284f655ef61f
coral-sparrow <mahmoud.shabban469@gmail.com>
2023-11-28 22:14:20 +0200

    adding cp program

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,25 +1,2 @@
 #!/usr/bin/python3
-print('The Zen of Python, by Tim Peters\n')
-b = 'is better than'
-i = 'If the implementation is' 
-a = 'Although'
-n = 'never'
-print(f'''Beautiful {b} ugly.
-Explicit {b} implicit.
-Simple {b} complex.
-Complex {b} complicated.
-Flat {b} nested.
-Sparse {b} dense.
-Readability counts.
-Special cases aren't special enough to break the rules.
-{a} practicality beats purity.
-Errors should {n} pass silently.
-Unless explicitly silenced.
-In the face of ambiguity, refuse the temptation to guess.
-There should be one-- and preferably only one --obvious way to do it.
-{a} that way may not be obvious at first unless you're Dutch.
-Now {b} {n}.
-{a} {n} is often better than *right* now.
-{i} hard to explain, it's a bad idea.
-{i} easy to explain, it may be a good idea.
-Namespaces are one honking great idea -- let's do more of those!''')
+import this
