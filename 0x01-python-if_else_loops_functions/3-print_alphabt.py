adb53076864d09e810ae20fab1b2c8a449f29733
coral-sparrow <mahmoud.shabban469@gmail.com>
2023-11-29 01:09:53 +0200

    adding cp program

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+for i in range(97, 123):
+    if i not in [113, 101]:
+        print(chr(i), end='')
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    my_str = ''
+    for i in str:
+        if ord(i) not in range(97, 123):
+            my_str += i
+        else:
+            my_str += chr(ord(i) - 32)
+    print(my_str)
