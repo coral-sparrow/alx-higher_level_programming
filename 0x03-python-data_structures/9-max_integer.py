c22f7a3eba9c44000a135f5c715ae853b12e3e50
coral-sparrow <mahmoud.shabban469@gmail.com>
2023-12-05 23:30:23 +0200

    ttsdf

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,6 +2,8 @@
 
 def max_integer(my_list=[]):
     if my_list:
-        return my_list.sorted()[-1]
+        c = my_list.copy()
+        c.sort()
+        return c[-1]
     else:
         return None
