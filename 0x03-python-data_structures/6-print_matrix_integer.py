66e700dc663581d116d5a774072013251ee08f2f
coral-sparrow <mahmoud.shabban469@gmail.com>
2023-12-05 22:44:58 +0200

    ttsdf

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    if matrix and len(matrix) > 0:
+    if matrix and len(matrix) > 1:
         for i in matrix:
             for j in range(len(i)):
                 if j == len(i) - 1:
                     print('{:d}'.format(i[j]), end='\n')
                 else:
                     print('{:d}'.format(i[j]), end=' ')
+    else:
         print()
+
