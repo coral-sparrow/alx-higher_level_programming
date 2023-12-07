6aab175bacca47835f5d790e79ba1aa5cf0a854b
coral-sparrow <mahmoud.shabban469@gmail.com>
2023-12-08 01:05:06 +0200

    ttsdf

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,19 +1,10 @@
 #!/usr/bin/python3
-
 def search_replace(my_list, search, replace):
     if my_list:
         t = []
         for i in range(len(my_list)):
             if my_list[i] == search:
                 t.append(replace)
-                i += 1
-                break
             else:
                 t.append(my_list[i])
-        if i == len(my_list) - 1:
-            return t
-        else:
-            t += my_list[i:]
-            return t
-    else:
-        return None
+        return t
