773c97e19bcdee495ae5f7fbceb200d71ca39a8b
coral-sparrow <mahmoud.shabban469@gmail.com>
2023-12-08 01:01:19 +0200

    ttsdf

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    t = []
-    for i in range(len(my_list)):
-        if my_list[i] == search:
-            t.append(replace)
-            i += 1
-            break
+    if my_list:
+        t = []
+        for i in range(len(my_list)):
+            if my_list[i] == search:
+                t.append(replace)
+                i += 1
+                break
+            else:
+                t.append(my_list[i])
+        if i == len(my_list) - 1:
+            return t
         else:
-            t.append(my_list[i])
-    if i == len(my_list) - 1:
-        return t
-    else:
-        t += my_list[i:]
-        return t
+            t += my_list[i:]
+            return t
