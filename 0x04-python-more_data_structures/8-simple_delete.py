1244e95fe64c498b965b1ed8579c01e17292bdea
coral-sparrow <mahmoud.shabban469@gmail.com>
2023-12-08 00:47:56 +0200

    ttsdf

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def simple_delete(a_dictionary, key=""):
-    if a_dictionary.get(key, None) != None:
+    if a_dictionary.get(key, None) is not None:
         return a_dictionary.pop(key)
