bc8172881c606ab447e10d67abadff16e7b52994
coral-sparrow <mahmoud.shabban469@gmail.com>
2023-12-20 17:43:08 +0200

    ttsdf

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    '''
+    this is empty class
+    '''
+    def __init__(self, size) -> None:
+        '''
+        this is consturctor
+        '''
+        self.__size = size
