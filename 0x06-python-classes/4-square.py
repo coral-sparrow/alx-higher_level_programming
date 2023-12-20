674070f7ff7e616f00d5431eff186936ab2fca88
coral-sparrow <mahmoud.shabban469@gmail.com>
2023-12-21 00:44:07 +0200

    ttsdf

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    '''
+    this is empty class
+    '''
+    def __init__(self, size=0) -> None:
+        '''
+        this is consturctor
+        '''
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        '''
+        this return area of the square
+        '''
+        return self.__size ** 2
