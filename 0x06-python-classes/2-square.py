bf14d1d748b57da57b9873c2166694ac7fdbaae8
coral-sparrow <mahmoud.shabban469@gmail.com>
2023-12-20 17:53:45 +0200

    ttsdf

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,7 +10,7 @@ class Square:
         '''
         this is consturctor
         '''
-        if isinstance(size, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
