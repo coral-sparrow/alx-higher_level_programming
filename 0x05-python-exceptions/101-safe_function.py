d2758ce7185804e62c06f6315561265acc383476
coral-sparrow <mahmoud.shabban469@gmail.com>
2023-12-21 03:09:29 +0200

    ttsdf

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,7 +5,7 @@ import sys
 def safe_function(fct, *args):
     """Define a class functions."""
     try:
-        fct()
+        return fct(*args)
     except:
         print(f"Exception: {sys.exc_info()[1]}", file=sys.stderr)
         return None
