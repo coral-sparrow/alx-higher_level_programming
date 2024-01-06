19532d7942e4c58fe6180a8525ebba08543a4da2
coral-sparrow <mahmoud.shabban469@gmail.com>
2024-01-06 12:34:56 +0200

    hash thing file 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Define a class Rectangle."""
+
+
+class Rectangle:
+    """Define a class Rectangle."""
+    def __init__(self, width=0, height=0) -> None:
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        '''return the width private attribute'''
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        '''sets the width private attribute'''
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        '''return the height private attribute'''
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        '''sets the height private attribute'''
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
