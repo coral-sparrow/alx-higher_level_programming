2a37911e693e1fc25e96d4faf1b1f8db70340d99
coral-sparrow <mahmoud.shabban469@gmail.com>
2024-01-06 13:01:46 +0200

    hash thing file 1

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -55,10 +55,10 @@ class Rectangle:
 
         if self.width == 0 or self.height == 0:
             return ''
-        
+
         st = ''
         for i in range(self.height):
-            if i == self.height -1:
+            if i == self.height - 1:
 
                 st += '#' * self.width
             else:
