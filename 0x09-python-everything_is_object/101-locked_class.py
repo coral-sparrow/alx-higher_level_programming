d22c0ef1c97e5be559e303f60dc14c47bdced9a0
coral-sparrow <mahmoud.shabban469@gmail.com>
2024-01-06 22:25:53 +0200

    hash thing file 1

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -8,10 +8,4 @@ class LockedClass:
     for anything but attributes called 'first_name'.
     """
 
-    def __setattr__(self, name: str, value: Any) -> None:
-        """ setattr Dunder method """
-        if name != 'first_name':
-            raise AttributeError((f"'LockedClass' object has no"
-                                  f"attribute '{name}'"))
-        else:
-            super().__setattr__(name, value)
+    __slots__ = ["first_name"]
