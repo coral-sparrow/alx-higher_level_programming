b736a18092d8ca82b13774536b823aea0efed7a1
coral-sparrow <mahmoud.shabban469@gmail.com>
2024-01-06 22:15:47 +0200

    hash thing file 1

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-''' LockedClass to control attributes creation '''
+""" LockedClass to control attributes creation """
 
 
 class LockedClass:
-    ''' The LockedClass '''
+    """ The LockedClass """
     def __setattr__(self, name: str, value: Any) -> None:
-        ''' setattr Dunder method '''
+        """ setattr Dunder method """
         if name != 'first_name':
             raise AttributeError((f"'LockedClass' object has no"
                                   f"attribute '{name}'"))
