581fd2ff4552bf452cdd4e630114b9d5d6bcfe11
coral-sparrow <mahmoud.shabban469@gmail.com>
2024-01-06 13:46:00 +0200

    hash thing file 1

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -64,9 +64,9 @@ class Rectangle:
         for i in range(self.height):
             if i == self.height - 1:
 
-                st += f'{Rectangle.print_symbol}' * self.width
+                st += f'{self.print_symbol}' * self.width
             else:
-                st += f'{Rectangle.print_symbol}' * self.width + '\n'
+                st += f'{self.print_symbol}' * self.width + '\n'
         return st
 
     def __repr__(self) -> str:
