c4d365c0c6a840fc71ffd1a511ed20f3c3f517c7
coral-sparrow <mahmoud.shabban469@gmail.com>
2024-01-06 22:17:02 +0200

    hash thing file 1

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-""" LockedClass to control attributes creation """
+"""Defines a locked class."""
 
 
 class LockedClass:
-    """ The LockedClass """
+    """
+    Prevent the user from instantiating new LockedClass attributes
+    for anything but attributes called 'first_name'.
+    """
     def __setattr__(self, name: str, value: Any) -> None:
         """ setattr Dunder method """
         if name != 'first_name':
