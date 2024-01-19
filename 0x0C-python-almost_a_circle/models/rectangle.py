09390d2322748926016e3dc5859761b0ec4c7f88
coral-sparrow <mahmoud.shabban469@gmail.com>
2024-01-19 15:06:08 +0200

    hash thing file 1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,6 +6,7 @@ class Rectangle(Base):
     '''the Rectangle class the inherits from base'''
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        ''' init method of the rectangle class'''
         super().__init__(id)
         self.width = width
         self.height = height
@@ -14,32 +15,40 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        '''width getter'''
         return self.__width
 
     @width.setter
     def width(self, value):
+        '''width setter'''
         self.__width = value
 
     @property
     def height(self):
+        '''height getter'''
         return self.__height
 
     @height.setter
     def height(self, value):
+        '''height setter'''
         self.__height = value
 
     @property
     def x(self):
+        '''x getter'''
         return self.__x
 
     @x.setter
     def x(self, value):
+        '''x setter'''
         self.__x = value
 
     @property
     def y(self):
+        '''y getter'''
         return self.__y
 
     @y.setter
     def y(self, value):
+        '''y setter'''
         self.__y = value
