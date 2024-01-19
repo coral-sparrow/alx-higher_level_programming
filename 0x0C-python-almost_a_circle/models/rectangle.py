6e991ef7b17ba3d6a8d34df39838cea9b91f3399
coral-sparrow <mahmoud.shabban469@gmail.com>
2024-01-19 15:09:12 +0200

    hash thing file 1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,6 +7,7 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         ''' init method of the rectangle class'''
+
         super().__init__(id)
         self.width = width
         self.height = height
