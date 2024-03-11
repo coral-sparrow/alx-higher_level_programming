14e8fa3ea19fb895dbe568bae3e46a3d3a6bc481
coral-sparrow <mahmoud.shabban469@gmail.com>
2024-03-11 22:45:56 +0200

    js coding

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// import { argv } from 'node:process';
+// const { argv } = require('node:process');
+
+const myArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (const i in myArr) {
+  console.log(myArr[i]);
+}
