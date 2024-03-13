7cdb6a814e591b29866007b56dff84eb4b8d2514
coral-sparrow <mahmoud.shabban469@gmail.com>
2024-03-13 23:29:24 +0200

    js coding

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  const newList = [];
+
+  let j = 0;
+  for (let i = list.length - 1; i >= 0; i--) {
+    newList[j] = list[i];
+    j++;
+  }
+
+  return newList;
+};
