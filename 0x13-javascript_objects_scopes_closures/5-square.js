3df90402e6f5c3ad309e46b00511994195fdf70c
coral-sparrow <mahmoud.shabban469@gmail.com>
2024-03-13 23:09:29 +0200

    js coding

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,33 +1,10 @@
 #!/usr/bin/node
 
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      let raw = '';
-      for (let j = 0; j < this.width; j++) {
-        raw += 'X';
-      }
-      console.log(raw);
-    }
-  }
-
-  rotate () {
-    const tmp = this.width;
-    this.width = this.height;
-    this.height = tmp;
-  }
-
-  duple () {
-    this.widht *= 2;
-    this.height *= 2;
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
   }
 }
 
-module.exports = Rectangle;
+module.exports = Square;
