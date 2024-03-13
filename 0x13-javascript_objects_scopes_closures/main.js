2c0d815ed559fd1b3766e5ad680d9718a1866c74
coral-sparrow <mahmoud.shabban469@gmail.com>
2024-03-13 23:00:45 +0200

    js coding

--- 0x13-javascript_objects_scopes_closures/main.js
+++ 0x13-javascript_objects_scopes_closures/main.js
@@ -1,8 +1,18 @@
 #!/usr/bin/node
-const Rectangle = require('./3-rectangle');
+const Rectangle = require('./4-rectangle');
 
 const r1 = new Rectangle(2, 3);
+console.log('**** before rotate *****');
+r1.print();
+console.log('**** after rotate *****');
+r1.rotate();
 r1.print();
 
 const r2 = new Rectangle(10, 5);
+
+console.log('**** before douple *****');
+r2.print();
+
+console.log('**** after douple *****');
+r2.duple();
 r2.print();
