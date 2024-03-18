1f4752b1a8e4afb7247408707859e3cfec9dc49c
coral-sparrow <mahmoud.shabban469@gmail.com>
2024-03-18 14:01:12 +0200

    python db

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,28 +1,36 @@
 #!/usr/bin/python3
 '''this is mysql db with python operations'''
 
+
 import MySQLdb
 import sys
 
-uname = sys.argv[1]
-password = sys.argv[2]
-db = sys.argv[3]
-host = "localhost"
 
-con = MySQLdb.connect(
-    user=uname,
-    password=password,
-    database=db,
-    host=host
-    )
+def connectToDB() -> None:
+    '''connect to db func'''
+    uname = sys.argv[1]
+    password = sys.argv[2]
+    db = sys.argv[3]
+    host = "localhost"
 
-cur = con.cursor()
-cur.execute(
-    '''
-    select * from states
-    order by  id desc
-    '''
-    )
+    con = MySQLdb.connect(
+        user=uname,
+        password=password,
+        database=db,
+        host=host
+        )
 
-for i in cur.fetchall():
-    print(i)
+    cur = con.cursor()
+    cur.execute(
+        '''
+        select * from states
+        order by  id desc
+        '''
+        )
+
+    for i in cur.fetchall():
+        print(i)
+
+
+if __name__ == "__main__":
+    connectToDB()
