38b4a9b29dcefe6dcc75c0ebb93556d6280da586
coral-sparrow <mahmoud.shabban469@gmail.com>
2024-03-18 15:50:07 +0200

    python db

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+'''this is mysql db with python operations'''
+
+
+import MySQLdb
+import sys
+
+
+def connectToDB() -> None:
+    '''connect to db func'''
+    uname = sys.argv[1]
+    password = sys.argv[2]
+    db = sys.argv[3]
+    host = "localhost"
+
+    con = MySQLdb.connect(
+        user=uname,
+        password=password,
+        database=db,
+        host=host
+        )
+
+    cur = con.cursor()
+    cur.execute(
+        '''
+        select * from cities
+        order by  id asc
+        '''
+        )
+
+    for i in cur.fetchall():
+        print(i)
+
+    cur.close()
+    con.close()
+
+
+if __name__ == "__main__":
+    connectToDB()
