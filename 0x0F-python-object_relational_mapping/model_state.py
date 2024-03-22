2aa65c133e415eaf1aeb834ab97b0e57164be313
coral-sparrow <mahmoud.shabban469@gmail.com>
2024-03-22 16:52:00 +0200

    python db

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -12,6 +12,7 @@ def connectToDB():
     Base = declarative_base()
 
     class State(Base):
+        '''State mapper to states table'''
         __tablename__ = 'states'
         id = Column(Integer, primary_key=True, nullable=False)
         name = Column(String(128))
