c1f8c5c1649be1077a8f3242d3f0a8d784c83cd9
coral-sparrow <mahmoud.shabban469@gmail.com>
2024-03-22 21:10:12 +0200

    python db

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -13,5 +13,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for instance in session.query(State).order_by(State.id).filter(State.name.ilike('%a%')):
+    query = session.query(State).order_by(State.id).filter(
+        State.name.ilike('%a%'))
+    for instance in query:
         print(instance.id, instance.name, sep=": ")
