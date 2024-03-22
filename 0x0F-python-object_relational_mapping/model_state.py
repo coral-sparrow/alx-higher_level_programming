40af1930024013c43982f0a2d2712edcc2cfee8c
coral-sparrow <mahmoud.shabban469@gmail.com>
2024-03-22 16:54:51 +0200

    python db

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -6,19 +6,20 @@ from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
 
-def connectToDB():
-    '''connect to db code'''
-    engine = create_engine('mysql+pymysql://root:root@localhost/hbtn_0e_6_usa')
-    Base = declarative_base()
-
-    class State(Base):
-        '''State mapper to states table'''
-        __tablename__ = 'states'
-        id = Column(Integer, primary_key=True, nullable=False)
-        name = Column(String(128))
-
-    Base.metadata.create_all(engine)
+# def connectToDB():
+#     '''connect to db code'''
+# engine = create_engine('mysql+pymysql://root:root@localhost/hbtn_0e_6_usa')
+Base = declarative_base()
 
 
-if __name__ == "__main__":
-    connectToDB()
+class State(Base):
+    '''State mapper to states table'''
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128))
+
+# Base.metadata.create_all(engine)
+
+
+# if __name__ == "__main__":
+    # connectToDB()
