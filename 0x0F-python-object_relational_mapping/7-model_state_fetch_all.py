9c8881d759819e8e2d2664e513e7eca92881717a
coral-sparrow <mahmoud.shabban469@gmail.com>
2024-03-22 17:21:20 +0200

    python db

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''this is mysql db with python operations'''
+
+
+from sqlalchemy import create_engine
+# from sqlalchemy.ext.declarative import declarative_base
+import sys
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+def connectToDB():
+    '''connect to db func'''
+
+    uname = sys.argv[1]
+    password = sys.argv[2]
+    db = sys.argv[3]
+    host = "localhost"
+
+    engine = create_engine(f'mysql+pymysql://{uname}:{password}@{host}/{db}')
+
+    session = sessionmaker(bind=engine)
+
+    for instance in session.query(State).order_by(State.id):
+        print(f'{instance.id}: {instance.name}')
+
+
+if __name__ == "__main__":
+    connectToDB()
--- 0x0F-python-object_relational_mapping/sqlalchmytest.py
+++ 0x0F-python-object_relational_mapping/sqlalchmytest.py
@@ -3,7 +3,9 @@
 ##!/usr/bin/python3
 
 from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy.orm import declarative_base
+# from sqlalchemy.orm import declarative_base
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
 
 uname = 'root'
 password = 'root'
@@ -14,14 +16,19 @@ host = "192.168.1.4"
 
 def connectToDB():
     '''connect to db code'''
-    engine = create_engine('mysql+pymysql://root:root@192.168.1.4/alx_db', echo=True)
-    Base = declarative_base()
+    engine = create_engine(f'mysql+pymysql://{uname}:{password}@{host}/{db}', echo = True)
+    Session = sessionmaker(bind=engine)
 
-    class State(Base):
-        __tablename__ = 'states'
-        id = Column(Integer, primary_key=True, nullable=False)
-        name =  Column(String(50))
+    session = Session()
+    for instance in session.query(State).order_by(State.id):
+        print (f'{instance.id}: {instance.name}')
+    # Base = declarative_base()
 
-    Base.metadata.create_all(engine)
+    # class State(Base):
+    #     __tablename__ = 'states'
+    #     id = Column(Integer, primary_key=True, nullable=False)
+    #     name =  Column(String(50))
+
+    # Base.metadata.create_all(engine)
 
 connectToDB()
