27df88d4a3b5f7d66cdf86fce12fbd682da21ce2
coral-sparrow <mahmoud.shabban469@gmail.com>
2024-03-22 20:57:42 +0200

    python db

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,30 +1,17 @@
 #!/usr/bin/python3
-'''this is mysql db with python operations'''
-
-
-from sqlalchemy import create_engine
-# from sqlalchemy.ext.declarative import declarative_base
+"""Start link class to table in database
+"""
 import sys
-from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
-
-
-def connectToDB():
-    '''connect to db func'''
-
-    uname = sys.argv[1]
-    password = sys.argv[2]
-    db = sys.argv[3]
-    host = "localhost"
-
-    engine = create_engine(f'mysql+pymysql://{uname}:{password}@{host}:3306/{db}')
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-
-    session = Session()
-    for instance in session.query(State).order_by(State.id):
-        print(f'{instance.id}: {instance.name}')
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    connectToDB()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State).order_by(State.id):
+        print(instance.id, instance.name, sep=": ")
